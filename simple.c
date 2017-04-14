fb20e1f2c5bd9a2a3b75ab0f7dbcac83ef22adbc
unknown <>
2017-04-14 22:41:15 +0800

    little

--- simple.c
+++ simple.c
@@ -51,49 +51,67 @@ void simplemid(uint8 **image_array)
     }
 
     //单独扫描左线
+    
     uint8 left_i, limit_y0, limit_y1;
     uint8 scan_direct = 1;
-    for(left_i = i; ; (left_i += scan_direct))
+    uint8 n = 0;
+    while( n < point_count )
     {
-        // 找到任何一个点
-        int flag_left = 0;
-        
-        // left_j = ( left_line[line_p-1].y - limit_y ) >= 0? -limit_y
-        limit_y0 = (left_line[line_cnt_left-1].y - limit_y) > 0 ? left_line[line_cnt_left-1].y : 1;
-        limit_y1 = (left_line[line_cnt_left-1].y + limit_y) <= width - 1 ? left_line[line_cnt_left-1].y : width - 1;
-        
-        for( ; limit_y0 <= limit_y1; limit_y0++)
-        {
-            if(scan_direct == 1)
-            {
-                if(image_array[left_i][limit_y0] > image_array[left_i][limit_y0-1])
-                {
-                    flag_left = 1;
-                    left_line[line_cnt_left].x = left_i;
-                    left_line[line_cnt_left].y = limit_y0;
-                    line_cnt_left++;
-                }
-            }
-            else
-            {
-                if(image_array[left_i][limit_y0] < image_array[left_i][limit_y0-1])
-                {
-                    flag_left = 1;
-                    left_line[line_cnt_left].x = left_i;
-                    left_line[line_cnt_left].y = limit_y0 - 1;
-                    line_cnt_left++;
-                }
-            }
+        line_get_left();
+        line_get_right();
 
-        }
-        //没有找到点,反向查找
-        if(!flag_left){
-            scan_direct = -1;
-            left_line[line_cnt_left].x = left_line[line_cnt_left - 1].x;
-            left_line[line_cnt_left].y = left_line[line_cnt_left - 1].y;
-        }  
+
+
+
+
+
 
     }
+
+    
+
+
+    // for(left_i = i; ; (left_i += scan_direct))
+    // {
+    //     // 找到任何一个点
+    //     int flag_left = 0;
+        
+    //     // left_j = ( left_line[line_p-1].y - limit_y ) >= 0? -limit_y
+    //     limit_y0 = (left_line[line_cnt_left-1].y - limit_y) > 0 ? left_line[line_cnt_left-1].y : 1;
+    //     limit_y1 = (left_line[line_cnt_left-1].y + limit_y) <= width - 1 ? left_line[line_cnt_left-1].y : width - 1;
+        
+    //     for( ; limit_y0 <= limit_y1; limit_y0++)
+    //     {
+    //         if(scan_direct == 1)
+    //         {
+    //             if(image_array[left_i][limit_y0] > image_array[left_i][limit_y0-1])
+    //             {
+    //                 flag_left = 1;
+    //                 left_line[line_cnt_left].x = left_i;
+    //                 left_line[line_cnt_left].y = limit_y0;
+    //                 line_cnt_left++;
+    //             }
+    //         }
+    //         else
+    //         {
+    //             if(image_array[left_i][limit_y0] < image_array[left_i][limit_y0-1])
+    //             {
+    //                 flag_left = 1;
+    //                 left_line[line_cnt_left].x = left_i;
+    //                 left_line[line_cnt_left].y = limit_y0 - 1;
+    //                 line_cnt_left++;
+    //             }
+    //         }
+
+    //     }
+    //     //没有找到点,反向查找
+    //     if(!flag_left){
+    //         scan_direct = -1;
+    //         left_line[line_cnt_left].x = left_line[line_cnt_left - 1].x;
+    //         left_line[line_cnt_left].y = left_line[line_cnt_left - 1].y;
+    //     }  
+
+    // }
 
 
     
@@ -101,7 +119,11 @@ void simplemid(uint8 **image_array)
 
 
 
+bool line_get_left(uint8 n,)
+{
 
+
+}
 
 
 
